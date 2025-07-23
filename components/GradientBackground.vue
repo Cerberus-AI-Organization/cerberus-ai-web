d97a371459a756b5634c08d10bc348b822282517
sobotat <tomas.sobota@gmail.com>
2025-07-23 14:12:08 +0200

    Created Gradient Background and Text

---
+++ components/GradientBackground.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid justify-center bg-white dark:bg-black relative overflow-hidden">
+    <div class="z-10 col-start-1 row-start-1">
+      <slot></slot>
+    </div>
+    <div class="col-start-1 row-start-1">
+      <div 
+        :class="[
+          `absolute w-96 h-96 top-20 left-3/5`,
+          `bg-radial from-purple-600 to-sky-400`,
+          `rounded-full opacity-30 blur-3xl`
+        ]"
+      ></div>
+      <div 
+        :class="[
+          `absolute w-full h-full`,
+          `bg-gradient-to-t from-purple-600 via-sky-400 to-transparent`,
+          `opacity-10 blur-3xl`
+        ]"
+      ></div>
+     </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
---
+++ components/GradientText.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gradient-to-r from-purple-600 to-sky-400 bg-clip-text text-transparent">
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  text: String
+}>()
+</script>
+
+<style>
+
+</style>
