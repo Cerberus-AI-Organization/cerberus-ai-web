6e48121b559bafbbefa88400a638064af74da8d8
sobotat <tomas.sobota@gmail.com>
2025-07-23 14:12:42 +0200

    Fixed size of logo in appbar

--- components/home/HomeAppBar.vue
+++ components/home/HomeAppBar.vue
@@ -1,18 +1,16 @@
 <template>
   <UContainer class="py-6">
       <div class="flex items-center justify-between">
-        <!-- Logo a název -->
         <div class="flex items-center space-x-3">
-          <div class="w-12 h-12 rounded-lg">
+          <div class="w-10 h-10 md:w-12 md:h-12 rounded-lg">
             <NuxtImg 
               src="/icon.png" 
               alt="CerberusAI Logo" 
               class="w-full h-full object-cover" /> 
           </div>
-          <h1 class="text-2xl font-bold text-gray-900 dark:text-white">CerberusAI</h1>
+          <h1 class="flex text-2xl font-bold text-gray-900 dark:text-white">Cerberus<GradientText text="AI"/></h1>
         </div>
         
-        <!-- Tlačítka -->
         <div class="flex items-center space-x-3">
           <UButton
             icon="heroicons:chart-bar-square"
