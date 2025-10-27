7aa178f4d58b1785c041a1b19c32e79ad165c472
sobotat <tomas.sobota@gmail.com>
2025-10-27 19:24:07 +0100

    Implemented Theme into AccessDeniedPage.tsx

--- src/routes/access_denied/AccessDeniedPage.tsx
+++ src/routes/access_denied/AccessDeniedPage.tsx
@@ -1,9 +1,11 @@
 import {Button, Result} from 'antd';
 import {ExclamationCircleFilled} from '@ant-design/icons'
 import {useNavigate} from 'react-router-dom';
+import {useTheme} from "../../states/ThemeContext.tsx";
 
 function AccessDeniedPage() {
   const navigate = useNavigate();
+  const {themeColors} = useTheme();
 
   return (
     <div style={{
@@ -13,16 +15,29 @@ function AccessDeniedPage() {
       minHeight: '100vh',
       padding: '1rem',
       boxSizing: 'border-box',
+      backgroundColor: themeColors.backgroundSecondary,
     }}>
       <Result
         icon={
           <ExclamationCircleFilled style={{fontSize: 48, color: 'orange'}}/>
         }
-        title="Unauthorized"
-        subTitle="Sorry, you are not authorized to access this page."
+        title={
+          <span style={{color: themeColors.text}}>
+            "Unauthorized"
+          </span>
+        }
+        subTitle={
+          <span style={{color: themeColors.text}}>
+          "Sorry, you are not authorized to access this page."
+        </span>
+        }
         extra={
-          <Button type="primary" onClick={() => navigate('/')}>
-            Back to Home
+          <Button style={{
+            background: themeColors.accent,
+            borderColor: themeColors.accent
+          }}
+                  onClick={() => navigate('/')}>
+            <span style={{color: themeColors.text}}>Back to Home</span>
           </Button>
         }
       />
